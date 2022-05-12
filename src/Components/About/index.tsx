66076dfed8fb4a1432ac69df579cbcab7413ca62
roelineburger <93773777+roelineburger@users.noreply.github.com>
2022-05-12 11:23:54 +0200

    CSS styling of components and copy

--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "./1.jpeg";
+import img from "./2.jpeg";
 import "./styles.css";
 
 const About = () => {
@@ -9,7 +9,21 @@ const About = () => {
         <img className="about-img" src={img} />
         <article className="about-text">
           <h3 className="about-title">MY WHY</h3>
-          <p></p>
+          <p className="about-text-p">
+            “It is not the critic who counts: not the man who points out how the
+            strong man stumbles or where the doer of deeds could have done
+            better. The credit belongs to the man who is actually in the arena,
+            whose face is marred by dust and sweat and blood, who strives
+            valiantly, who errs and comes up short again and again, because
+            there is no effort without error or shortcoming, but who knows the
+            great enthusiasms, the great devotions, who spends himself in a
+            worthy cause; who, at the best, knows, in the end, the triumph of
+            high achievement, and who, at the worst, if he fails, at least he
+            fails while daring greatly, so that his place shall never be with
+            those cold and timid souls who knew neither victory nor defeat.”
+            —Theodore Roosevelt Speech at the Sorbonne, Paris, April 23, 1910
+          </p>
+          <p className="about-text-p2">This is me daring greatly.</p>
         </article>
         <p></p>
       </div>
--- src/Components/Home-Banner/index.tsx
+++ src/Components/Home-Banner/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import img from "./1.jpeg";
+import img from "./6.jpg";
+import img2 from "./10.jpg";
+import img3 from "./11.jpg";
 import "./styles.css";
 
 const HomeBanner = () => {
   return (
     <div className="banner">
       <article className="banner-text">
-        <h2>Hi! I'm Roeline Burger</h2>
-        <h2>Fullstack Software Developer</h2>
-        <p>akdlkjdlaskdjlaskdlasknflasjfalskjfalskjf</p>
+        <h2 className="banner-text-header1">Hi! I'm Roeline Burger</h2>
+        <h2 className="banner-text-header2">FULLSTACK SOFTWARE DEVELOPER</h2>
+        <p className="banner-text-p">
+          Inquisitive. Creator. Life-long learner.{" "}
+        </p>
       </article>
-      <img className="banner-image" src={img} alt="logo" />
+      <img className="banner-image" src={img} />
+      <img className="banner-image2" src={img2} />
+      <img className="banner-image3" src={img3} />
     </div>
   );
 };
